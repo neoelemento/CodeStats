1791424b99b4434a4827db0d39f0734a6a8f0427
Vishnu Padmanabhan <vishnu.aol27@gmail.com>
2015-06-01 18:16:21 +0530

    cleanup

--- app.js
+++ app.js
@@ -23,31 +23,3 @@ var CodeApp = angular.module('CodeApp', [])
 			return strg.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 		};
 	});
-
-
-
-//(function($) {
-//	var Code = {
-//		url: "http://codeivate.com/users/neoelemento.json",
-//		init: function() {
-//			this.getData();
-//		},
-//		getData: function() {
-//			$.ajax({
-//			     url:this.url,
-//			     dataType: 'jsonp',
-//			     success:function(json) {
-//			        $("#details").append('<ul><li>Username: ' + json.name + '</li><li>Days Coded: ' + json.total_days_coded + '</li><li>Longest streak: ' + parseInt((json.max_streak)/60) + ' mins</li><li>Number of known languages: ' + Object.keys(json.languages).length + '</li></ul>');
-//			     },
-//			     error:function() {
-//			         return "Error reading data";
-//			     }      
-//			});
-//		},
-//		putData: function () {
-//			//$('#username').append((this.getData()).username);
-//		}
-//	};
-//	
-//	Code.init();
-//})(jQuery);
